039c7197650c27d39ff63f6d80c5f5cb15d3282f
gaziles <eliza.gajda@student.wat.edu.pl>
2026-04-16 13:52:33 +0200

    wip: prepared read functionality

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
-
-
 users: list = [
-    {'username':'oliwia', 'location':'łódź', 'posts':1,'usermessage':['życzenia1', 'kocham legie','sprzedam opla', 'kiwi']},
-    {'username':'paweł', 'location':'ostróda', 'posts':2,'usermessage':['życzenia2', 'kocham legie1', 'sprzedam opla1']},
-    {'username':'eliza', 'location':'radom', 'posts':3,'usermessage':['życzenia3', 'kocham legie2',]},
-    {'username':'filip', 'location':'deblin', 'posts':4,'usermessage':['życzenia4', 'kocham legie3', 'sprzedam opla3', 'kiwi3']},
+    {'username': 'oliwia', 'location': 'łódź', 'posts': 1,
+     'usermessage': ['życzenia1', 'kocham legie', 'sprzedam opla', 'kiwi']},
+    {'username': 'paweł', 'location': 'ostróda', 'posts': 2,
+     'usermessage': ['życzenia2', 'kocham legie1', 'sprzedam opla1']},
+    {'username': 'eliza', 'location': 'radom', 'posts': 3, 'usermessage': ['życzenia3', 'kocham legie2', ]},
+    {'username': 'filip', 'location': 'deblin', 'posts': 4,
+     'usermessage': ['życzenia4', 'kocham legie3', 'sprzedam opla3', 'kiwi3']},
 ]
 
-for user in users[1:]:
-    print(f'Twój znajomy {user['username']} z miejscowości {user['location']} opublikował {user['posts']} wiadomości. Ostatnia wiadomość {user['usermessage'][-1]}')
+
+def read_data(users_data: list) -> None:
+    for user in users_data:
+        print(
+            f'Twój znajomy {user['username']} z miejscowości {user['location']} opublikował {user['posts']} wiadomości. Ostatnia wiadomość {user['usermessage'][-1]}')
 
 
-#     twój znajomy filip z miejscowości dęblin opublikował 1 post o treści: życzenia
+read_data(users[1:])
