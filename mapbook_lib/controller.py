9947c1d57234f6f4726c37ba5b4a80cbb92f89af
gaziles <eliza.gajda@student.wat.edu.pl>
2026-04-30 13:42:22 +0200

    feat: add update function

--- mapbook_lib/controller.py
+++ mapbook_lib/controller.py
@@ -12,9 +12,20 @@ def add_user(users_data: list) -> None:
     users_data.append({'username': name, 'location': location, 'posts': posts,
                        'usermessage': usermessage}, )
 
+
 def remove_user(users_data: list) -> None:
     name = input('Podaj imię użytkownika do usunięcia: ')
 
     for user in users_data:
         if user['username'] == name:
             users_data.remove(user)
+
+
+def update_user(users_data: list) -> None:
+    name = input('Podaj imię użytkownika do zmiany: ')
+
+    for user in users_data:
+        if user['username'] == name:
+            user['username'] = input('Podaj nowe imię: ')
+            user['location'] = input('Podaj nową lokalizację: ')
+            user['posts'] = int(input('Podaj nową liczbę postów: '))
